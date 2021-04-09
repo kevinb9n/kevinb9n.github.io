3194694443ba60732bb85f3c00c6bf2981caf21c
Kevin Bourrillion <kevinb9n@gmail.com>
2021-04-08 20:53:04 -0700

    make dumb test compile (still fail)

--- kevinb9n/src/test/java/site/kevinb9n/tmsoloplacement/BasicTest.kt
+++ kevinb9n/src/test/java/site/kevinb9n/tmsoloplacement/BasicTest.kt
@@ -7,10 +7,12 @@ import site.kevinb9n.tmsoloplacement.MarsMap.HexArea
 class BasicTest {
   @Test
   fun `try some shit`() {
-    val noctis : HexArea = THARSIS.get(5, 3)!!
-    val neighbors = THARSIS.neighbors(noctis)
-    assertThat(neighbors).hasSize(6)
-    assertThat(neighbors.filter { it.isReserved }).containsExactly(HexArea(5, 4, true, "PP"))
+      val noctis : HexArea = THARSIS.get(5, 3)!!
+      val neighbors = noctis.neighbors()
+      assertThat(neighbors).hasSize(6)
+      // fails with (non-equal instance of same class with same string representation)
+      assertThat(neighbors.filter { it.isReserved() }).containsExactly(
+              HexArea(THARSIS, 5, 4, MarsMap.AreaType.NOCTIS, "WPP"))
 
   }
 }
