551a6e291669746f7f8f85f75a5a64cf6b9652e5
Kevin Bourrillion <kevinb9n@gmail.com>
2022-10-20 20:55:05 -0700

    got ComplexViz mostly working but implemented VERY badly

--- kevinb9n/src/main/java/site/kevinb9n/javafx/ComplexViz.kt
+++ kevinb9n/src/main/java/site/kevinb9n/javafx/ComplexViz.kt
@@ -12,6 +12,7 @@ import javafx.scene.text.Text
 import javafx.scene.text.TextAlignment
 import javafx.stage.Stage
 import site.kevinb9n.math.Complex
+import site.kevinb9n.math.Complex.Companion.fromRe
 
 fun main() = Application.launch(ComplexViz::class.java)
 
@@ -32,8 +33,51 @@ class ComplexViz : Application() {
         it.stroke = Color.DARKGRAY
         it.strokeWidth = STROKE_WIDTH
       },
+      Circle(400.0, 400.0, 80.0). also {
+        it.stroke = Color.LIGHTGRAY
+        it.strokeWidth = STROKE_WIDTH * 2 / 3
+        it.fill = Color.TRANSPARENT
+      },
+      Line().also {
+        it.startX = 400.0
+        it.startY = 400.0
+        it.stroke = Color.DARKBLUE
+        it.strokeWidth = STROKE_WIDTH
+        it.endXProperty().bindDouble(aProp, bProp) {
+          complexToCoord(aProp.value + bProp.value).first
+        }
+        it.endYProperty().bindDouble(aProp, bProp) {
+          complexToCoord(aProp.value + bProp.value).second
+        }
+      },
+      Line().also {
+        it.startX = 400.0
+        it.startY = 400.0
+        it.stroke = Color.DARKORANGE
+        it.strokeWidth = STROKE_WIDTH
+        it.endXProperty().bindDouble(aProp, bProp) {
+          complexToCoord(aProp.value * bProp.value).first
+        }
+        it.endYProperty().bindDouble(aProp, bProp) {
+          complexToCoord(aProp.value * bProp.value).second
+        }
+      },
+      Line().also {
+        it.startX = 400.0
+        it.startY = 400.0
+        it.stroke = Color.PURPLE
+        it.strokeWidth = STROKE_WIDTH
+        it.endXProperty().bindDouble(aProp, bProp) {
+          val b = if (bProp.value?.abs() ?: 0.0 < 1e-7) fromRe(1e-7) else bProp.value
+          complexToCoord(aProp.value / b).first
+        }
+        it.endYProperty().bindDouble(aProp, bProp) {
+          val b = if (bProp.value?.abs() ?: 0.0 < 1e-7) fromRe(1e-7) else bProp.value
+          complexToCoord(aProp.value / b).second
+        }
+      },
       Group(
-        Circle(0.0, 0.0, 9.0).also {
+        Circle(0.0, 0.0, 11.0).also {
           it.fill = Color.DARKBLUE
         },
         Text("+").also {
@@ -41,7 +85,7 @@ class ComplexViz : Application() {
           it.fill = Color.WHITE
           it.textAlignment = TextAlignment.CENTER
           it.translateX = -4.0
-          it.translateY = 3.0
+          it.translateY = 4.0
         }
       ).also {
         it.translateXProperty().bindDouble(aProp, bProp) {
@@ -52,7 +96,7 @@ class ComplexViz : Application() {
         }
       },
       Group(
-        Circle(0.0, 0.0, 9.0).also {
+        Circle(0.0, 0.0, 11.0).also {
           it.fill = Color.DARKORANGE
         },
         Text("X").also {
@@ -60,7 +104,7 @@ class ComplexViz : Application() {
           it.fill = Color.WHITE
           it.textAlignment = TextAlignment.CENTER
           it.translateX = -4.0
-          it.translateY = 3.0
+          it.translateY = 4.0
         }
       ).also {
         it.translateXProperty().bindDouble(aProp, bProp) {
@@ -71,7 +115,7 @@ class ComplexViz : Application() {
         }
       },
       Group(
-        Circle(0.0, 0.0, 9.0).also {
+        Circle(0.0, 0.0, 11.0).also {
           it.fill = Color.PURPLE
         },
         Text("/").also {
@@ -79,21 +123,21 @@ class ComplexViz : Application() {
           it.fill = Color.WHITE
           it.textAlignment = TextAlignment.CENTER
           it.translateX = -4.0
-          it.translateY = 3.0
+          it.translateY = 4.0
         }
       ).also {
         it.translateXProperty().bindDouble(aProp, bProp) {
-          val b = if (bProp.value?.abs() ?: 0.0 < 1e-7) 1e-7 else bProp.value.abs()
+          val b = if (bProp.value?.abs() ?: 0.0 < 1e-7) fromRe(1e-7) else bProp.value
           complexToCoord(aProp.value / b).first
         }
         it.translateYProperty().bindDouble(aProp, bProp) {
-          val b = if (bProp.value?.abs() ?: 0.0 < 1e-7) 1e-7 else bProp.value.abs()
+          val b = if (bProp.value?.abs() ?: 0.0 < 1e-7) fromRe(1e-7) else bProp.value
           complexToCoord(aProp.value / b).second
         }
       },
       DragToTranslate(
         Group(
-          Circle(0.0, 0.0, 9.0).also {
+          Circle(0.0, 0.0, 11.0).also {
             it.fill = Color.DARKRED
           },
           Text("A").also {
@@ -101,7 +145,7 @@ class ComplexViz : Application() {
             it.fill = Color.WHITE
             it.textAlignment = TextAlignment.CENTER
             it.translateX = -4.0
-            it.translateY = 3.0
+            it.translateY = 4.0
           }
         ).also {
           it.translateX = 500.0
@@ -113,7 +157,7 @@ class ComplexViz : Application() {
       ),
       DragToTranslate(
         Group(
-          Circle(0.0, 0.0, 9.0).also {
+          Circle(0.0, 0.0, 11.0).also {
             it.fill = Color.DARKGREEN
           },
           Text("B").also {
@@ -121,7 +165,7 @@ class ComplexViz : Application() {
             it.fill = Color.WHITE
             it.textAlignment = TextAlignment.CENTER
             it.translateX = -4.0
-            it.translateY = 3.0
+            it.translateY = 4.0
           }
         ).also {
           it.translateX = 550.0
@@ -141,16 +185,16 @@ class ComplexViz : Application() {
   }
 
   fun coordToComplex(x: Double, y: Double): Complex {
-    val re = (x - 400.0) / 30.0
-    val im = (400.0 - y) / 30.0
+    val re = (x - 400.0) / 80.0
+    val im = (400.0 - y) / 80.0
     val c = Complex(re, im)
 //    println("x $x y $y --> c $c")
     return c
   }
 
   fun complexToCoord(c: Complex): Pair<Double, Double> {
-    val x = 400.0 + c.re * 30.0
-    val y = 400.0 - c.im * 30.0
+    val x = 400.0 + c.re * 80.0
+    val y = 400.0 - c.im * 80.0
 //    println("c $c --> x $x y $y")
     return x to y
   }
